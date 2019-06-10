db75cd29069baec1c71de288ab63cb1789b2c082
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 01:36:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/add'
+  resources :products, only: [:index, :add]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
